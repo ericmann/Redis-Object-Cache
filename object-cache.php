14cfe5b483c05e2f4cc2f08c44ccf6c3fd5a5259
Luke Woodward <luke.woodward@10up.com>
2014-05-16 00:18:12 -0700

    Don't use is_multisite unless it is defined

--- object-cache.php
+++ object-cache.php
@@ -370,8 +370,10 @@ class WP_Object_Cache {
 		}
 
 		// Assign global and blog prefixes for use with keys
-		$this->global_prefix = ( is_multisite() || defined( 'CUSTOM_USER_TABLE' ) && defined( 'CUSTOM_USER_META_TABLE' ) ) ? '' : $table_prefix;
-		$this->blog_prefix   = ( is_multisite() ? $blog_id : $table_prefix ) . ':';
+		if ( function_exists( 'is_multisite' ) ) {
+			$this->global_prefix = ( is_multisite() || defined( 'CUSTOM_USER_TABLE' ) && defined( 'CUSTOM_USER_META_TABLE' ) ) ? '' : $table_prefix;
+			$this->blog_prefix   = ( is_multisite() ? $blog_id : $table_prefix ) . ':';
+		}
 	}
 
 	/**
@@ -845,7 +847,7 @@ class WP_Object_Cache {
 	 * @return bool
 	 */
 	public function switch_to_blog( $_blog_id ) {
-		if ( ! is_multisite() ) {
+		if ( ! function_exists( 'is_multisite' ) || ! is_multisite() ) {
 			return false;
 		}
 
